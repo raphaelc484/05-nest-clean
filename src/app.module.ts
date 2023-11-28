66501fdca06acf9308f10838c0a9ad85be38a131
@rapha <raphaelc484@hotmail.com>
2023-11-28 13:06:18 -0300

    add config service for env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { PrismaService } from './prisma/prisma.service'
 import { CreateaccountController } from './controllers/create-account.controller'
+import { envSchema } from './env'
 
 @Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+  ],
   controllers: [CreateaccountController],
   providers: [PrismaService],
 })
