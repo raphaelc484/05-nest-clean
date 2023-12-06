4c9e9cc6da33c93ef3d689c4ae8209aad7588eb3
@rapha <raphaelc484@hotmail.com>
2023-12-05 21:59:09 -0300

    fix import errors

---
+++ src/infra/app.module.ts
@@ -0,0 +1,18 @@
+import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import { PrismaService } from './prisma/prisma.service'
+import { envSchema } from './env'
+import { AuthModule } from './auth/auth.module'
+import { HttpModule } from './http/http.module'
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      validate: (env) => envSchema.parse(env),
+      isGlobal: true,
+    }),
+    AuthModule,
+    HttpModule,
+  ],
+})
+export class AppModule {}
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,21 +1,11 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { PrismaService } from './prisma/prisma.service'
 import { CreateaccountController } from './controllers/create-account.controller'
-import { envSchema } from './env'
-import { AuthModule } from './auth/auth.module'
 import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { PrismaService } from '../prisma/prisma.service'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      validate: (env) => envSchema.parse(env),
-      isGlobal: true,
-    }),
-    AuthModule,
-  ],
   controllers: [
     CreateaccountController,
     AuthenticateController,
@@ -24,4 +14,4 @@ import { FetchRecentQuestionsController } from './controllers/fetch-recent-quest
   ],
   providers: [PrismaService],
 })
-export class AppModule {}
+export class HttpModule {}
